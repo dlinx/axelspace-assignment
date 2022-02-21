72bc24670c268384551a6b8418f47c84bec632e9
Dange Laxmikant <5583697+dlinx@users.noreply.github.com>
2022-02-21 11:06:13 +0900

    Added navigation buttons

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,12 +4,12 @@ import { MapEvent } from "../constants/MapData";
 
 type Props = {
   onListItemClicked: (listItem: MapEvent, index: number) => void
-  mapData: MapEvent[] 
+  mapData: MapEvent[]
 }
 const ListContainer = styled.ul`
   list-style: none;
   padding: 0;
-  margin: 10px 0px;
+  margin: 8px 0px;
   width: 300px;
   overflow: auto
 `;
@@ -41,16 +41,53 @@ const ListText = styled.span`
   cursor: default;
   user-select: none;
 `;
+const NavContainer = styled.div`
+  display: flex;
+`
+const NavigationButton = styled.button`
+  width: 60px;
+  height: 40px;
+`;
+const SortOptions = styled.select`
+  width: 100%;
+  border-radius: 0;
+`
 
 export const List: React.FC<Props> = (props) => {
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [mapData, setMapData] = useState(props.mapData);
 
   const onListItemClicked = (mapEvent: MapEvent, i: number) => {
     props.onListItemClicked(mapEvent, i);
     setSelectedIndex(i);
   }
+  const onNextButtonCLick = () => {
+    const nextIndex = (selectedIndex ?? 0) + 1;
+    setSelectedIndex(nextIndex);
+    props.onListItemClicked(mapData[nextIndex], nextIndex)
+  }
+  const onPreviousButtonClick = () => {
+    const prevIndex = (selectedIndex ?? 0) - 1;
+    setSelectedIndex(prevIndex);
+    props.onListItemClicked(mapData[prevIndex], prevIndex)
+  }
+
   return <ListContainer>
-    {props.mapData.map((mapEvent, i) => <ListItem key={i}
+    <NavContainer>
+      <NavigationButton
+        onClick={() => onPreviousButtonClick()}
+        disabled={selectedIndex === null || selectedIndex === 0}
+      >Prev</NavigationButton>
+      <SortOptions>
+        <option>Sort by Year</option>
+        <option>Sort Alphabetically</option>
+      </SortOptions>
+      <NavigationButton
+        onClick={() => onNextButtonCLick()}
+        disabled={selectedIndex === null || selectedIndex === mapData.length - 1}
+      >Next</NavigationButton>
+    </NavContainer>
+    {mapData.map((mapEvent, i) => <ListItem key={i}
       onClick={() => onListItemClicked(mapEvent, i)}
       isSelected={selectedIndex === i}>
       {mapEvent.properties.icon}
